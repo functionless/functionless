089403ddd2d8d895361bece30f36178030dde163
sam <sam@shapes.org>
2022-03-27 19:33:54 -0700

    chore: refactor tests and upgrade to node 16.14

--- test/step-function.test.ts
+++ test/step-function.test.ts
@@ -3929,3 +3929,464 @@ test("try { $SFN.map(list, (item) => task(item)) } catch { return null }", () =>
     },
   });
 });
+
+test("return $SFN.forEach(list, (item) => task(item))", () => {
+  const { stack, task } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", (list: string[]) => {
+    return $SFN.forEach(list, (item) => task(item));
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "return $SFN.forEach(list, function(item))",
+    States: {
+      "return $SFN.forEach(list, function(item))": {
+        End: true,
+        ItemsPath: "$.list",
+        Iterator: {
+          StartAt: "return task(item)",
+          States: {
+            "return task(item)": {
+              End: true,
+              Parameters: {
+                FunctionName: task.resource.functionName,
+                Payload: "$.item",
+              },
+              Resource: "arn:aws:states:::lambda:invoke",
+              ResultPath: "$",
+              Type: "Task",
+            },
+          },
+        },
+        Parameters: {
+          item: "$$.Map.Item.Value",
+        },
+        ResultPath: "$",
+        Type: "Map",
+      },
+    },
+  });
+});
+
+test("return $SFN.forEach(list, {maxConcurrency: 2} (item) => task(item))", () => {
+  const { stack, task } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", (list: string[]) => {
+    return $SFN.forEach(list, { maxConcurrency: 2 }, (item) => task(item));
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "return $SFN.forEach(list, {maxConcurrency: 2}, function(item))",
+    States: {
+      "return $SFN.forEach(list, {maxConcurrency: 2}, function(item))": {
+        End: true,
+        ItemsPath: "$.list",
+        MaxConcurrency: 2,
+        Iterator: {
+          StartAt: "return task(item)",
+          States: {
+            "return task(item)": {
+              End: true,
+              Parameters: {
+                FunctionName: task.resource.functionName,
+                Payload: "$.item",
+              },
+              Resource: "arn:aws:states:::lambda:invoke",
+              ResultPath: "$",
+              Type: "Task",
+            },
+          },
+        },
+        Parameters: {
+          item: "$$.Map.Item.Value",
+        },
+        ResultPath: "$",
+        Type: "Map",
+      },
+    },
+  });
+});
+
+test("$SFN.forEach(list, (item) => task(item))", () => {
+  const { stack, task } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", (list: string[]) => {
+    $SFN.forEach(list, (item) => task(item));
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "$SFN.forEach(list, function(item))",
+    States: {
+      "$SFN.forEach(list, function(item))": {
+        ItemsPath: "$.list",
+        Next: "return null",
+        Iterator: {
+          StartAt: "return task(item)",
+          States: {
+            "return task(item)": {
+              End: true,
+              Parameters: {
+                FunctionName: task.resource.functionName,
+                Payload: "$.item",
+              },
+              Resource: "arn:aws:states:::lambda:invoke",
+              ResultPath: "$",
+              Type: "Task",
+            },
+          },
+        },
+        Parameters: {
+          item: "$$.Map.Item.Value",
+        },
+        ResultPath: null,
+        Type: "Map",
+      },
+      "return null": {
+        End: true,
+        OutputPath: "$.null",
+        Parameters: {
+          null: null,
+        },
+        Type: "Pass",
+      },
+    },
+  });
+});
+
+test("result = $SFN.forEach(list, (item) => task(item))", () => {
+  const { stack, task } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", (list: string[]) => {
+    const result = $SFN.forEach(list, (item) => task(item));
+    return result;
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "result = $SFN.forEach(list, function(item))",
+    States: {
+      "result = $SFN.forEach(list, function(item))": {
+        ItemsPath: "$.list",
+        Iterator: {
+          StartAt: "return task(item)",
+          States: {
+            "return task(item)": {
+              End: true,
+              Parameters: {
+                FunctionName: task.resource.functionName,
+                Payload: "$.item",
+              },
+              Resource: "arn:aws:states:::lambda:invoke",
+              ResultPath: "$",
+              Type: "Task",
+            },
+          },
+        },
+        Next: "return result",
+        Parameters: {
+          item: "$$.Map.Item.Value",
+        },
+        ResultPath: "$.result",
+        Type: "Map",
+      },
+      "return result": {
+        End: true,
+        OutputPath: "$.result",
+        Type: "Pass",
+      },
+    },
+  });
+});
+
+test("return $SFN.forEach(list, (item) => try { task(item)) } catch { return null }", () => {
+  const { stack, task } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", (list: string[]) => {
+    return $SFN.forEach(list, (item) => {
+      try {
+        return task(item);
+      } catch {
+        return null;
+      }
+    });
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "return $SFN.forEach(list, function(item))",
+    States: {
+      "return $SFN.forEach(list, function(item))": {
+        End: true,
+        ItemsPath: "$.list",
+        Iterator: {
+          StartAt: "return task(item)",
+          States: {
+            "return task(item)": {
+              Catch: [
+                {
+                  ErrorEquals: ["States.ALL"],
+                  Next: "return null",
+                  ResultPath: null,
+                },
+              ],
+              End: true,
+              Parameters: {
+                FunctionName: task.resource.functionName,
+                Payload: "$.item",
+              },
+              Resource: "arn:aws:states:::lambda:invoke",
+              ResultPath: "$",
+              Type: "Task",
+            },
+            "return null": {
+              End: true,
+              OutputPath: "$.null",
+              Parameters: {
+                null: null,
+              },
+              Type: "Pass",
+            },
+          },
+        },
+        Parameters: {
+          item: "$$.Map.Item.Value",
+        },
+        ResultPath: "$",
+        Type: "Map",
+      },
+    },
+  });
+});
+
+test("try { $SFN.forEach(list, (item) => task(item)) } catch { return null }", () => {
+  const { stack, task } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", (list: string[]) => {
+    try {
+      return $SFN.forEach(list, (item) => task(item));
+    } catch {
+      return null;
+    }
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "return $SFN.forEach(list, function(item))",
+    States: {
+      "return $SFN.forEach(list, function(item))": {
+        Catch: [
+          {
+            ErrorEquals: ["States.ALL"],
+            Next: "return null",
+            ResultPath: null,
+          },
+        ],
+        End: true,
+        ItemsPath: "$.list",
+        Iterator: {
+          StartAt: "return task(item)",
+          States: {
+            "return task(item)": {
+              End: true,
+              Parameters: {
+                FunctionName: task.resource.functionName,
+                Payload: "$.item",
+              },
+              Resource: "arn:aws:states:::lambda:invoke",
+              ResultPath: "$",
+              Type: "Task",
+            },
+          },
+        },
+        Parameters: {
+          item: "$$.Map.Item.Value",
+        },
+        ResultPath: "$",
+        Type: "Map",
+      },
+      "return null": {
+        End: true,
+        OutputPath: "$.null",
+        Parameters: {
+          null: null,
+        },
+        Type: "Pass",
+      },
+    },
+  });
+});
+
+test('return $SFN.parallel(() => "hello", () => "world"))', () => {
+  const { stack } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", () => {
+    return $SFN.parallel(
+      () => "hello",
+      () => "world"
+    );
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "return $SFN.parallel([function(), function()])",
+    States: {
+      "return $SFN.parallel([function(), function()])": {
+        Branches: [
+          {
+            StartAt: 'return "hello"',
+            States: {
+              'return "hello"': {
+                End: true,
+                Result: "hello",
+                ResultPath: "$",
+                Type: "Pass",
+              },
+            },
+          },
+          {
+            StartAt: 'return "world"',
+            States: {
+              'return "world"': {
+                End: true,
+                Result: "world",
+                ResultPath: "$",
+                Type: "Pass",
+              },
+            },
+          },
+        ],
+        End: true,
+        ResultPath: "$",
+        Type: "Parallel",
+      },
+    },
+  });
+});
+
+test('try { return $SFN.parallel(() => "hello", () => "world")) } catch { return null }', () => {
+  const { stack } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", () => {
+    try {
+      return $SFN.parallel(
+        () => "hello",
+        () => "world"
+      );
+    } catch {
+      return null;
+    }
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "return $SFN.parallel([function(), function()])",
+    States: {
+      "return $SFN.parallel([function(), function()])": {
+        Branches: [
+          {
+            StartAt: 'return "hello"',
+            States: {
+              'return "hello"': {
+                End: true,
+                Result: "hello",
+                ResultPath: "$",
+                Type: "Pass",
+              },
+            },
+          },
+          {
+            StartAt: 'return "world"',
+            States: {
+              'return "world"': {
+                End: true,
+                Result: "world",
+                ResultPath: "$",
+                Type: "Pass",
+              },
+            },
+          },
+        ],
+        Catch: [
+          {
+            ErrorEquals: ["States.ALL"],
+            Next: "return null",
+            ResultPath: null,
+          },
+        ],
+        End: true,
+        ResultPath: "$",
+        Type: "Parallel",
+      },
+      "return null": {
+        Type: "Pass",
+        Parameters: {
+          null: null,
+        },
+        OutputPath: "$.null",
+        End: true,
+      },
+    },
+  });
+});
+
+test("return $SFN.parallel(() => try { task() } catch { return null })) }", () => {
+  const { stack, task } = initStepFunctionApp();
+  const definition = new ExpressStepFunction(stack, "fn", () => {
+    try {
+      return $SFN.parallel(() => {
+        try {
+          return task();
+        } catch {
+          return null;
+        }
+      });
+    } catch {
+      return null;
+    }
+  }).definition;
+
+  expect(definition).toEqual({
+    StartAt: "return $SFN.parallel([function()])",
+    States: {
+      "return $SFN.parallel([function()])": {
+        Branches: [
+          {
+            StartAt: "return task(null)",
+            States: {
+              "return task(null)": {
+                Catch: [
+                  {
+                    ErrorEquals: ["States.ALL"],
+                    Next: "return null",
+                    ResultPath: null,
+                  },
+                ],
+                End: true,
+                Parameters: {
+                  FunctionName: task.resource.functionName,
+                  Payload: null,
+                },
+                Resource: "arn:aws:states:::lambda:invoke",
+                ResultPath: "$",
+                Type: "Task",
+              },
+              "return null": {
+                End: true,
+                OutputPath: "$.null",
+                Parameters: {
+                  null: null,
+                },
+                Type: "Pass",
+              },
+            },
+          },
+        ],
+        Catch: [
+          {
+            ErrorEquals: ["States.ALL"],
+            Next: "return null 1",
+            ResultPath: null,
+          },
+        ],
+        End: true,
+        ResultPath: "$",
+        Type: "Parallel",
+      },
+      "return null 1": {
+        End: true,
+        OutputPath: "$.null",
+        Parameters: {
+          null: null,
+        },
+        Type: "Pass",
+      },
+    },
+  });
+});
