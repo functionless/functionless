ec77fce4be1c6df52d791944b42fb8744013600c
sam <sam@functionless.org>
2022-05-25 14:37:09 -0700

    chore: rebrand to Serverless with benefits

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Functionless",
-  tagline: "The Typesafe Cloud",
+  tagline: "Serverless with benefits",
   url: "https://functionless.org",
   baseUrl: "/",
   onBrokenLinks: "throw",
