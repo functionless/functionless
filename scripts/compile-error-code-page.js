8db758b8b15b1ee312dc8672c8cb9cfc989191e4
sam <sam@functionless.org>
2022-06-01 13:58:28 -0700

    fix: run doc generator during docusaurus build process

--- scripts/compile-error-code-page.js
+++ scripts/compile-error-code-page.js
@@ -3,6 +3,10 @@ const { ErrorCodes } = require("../lib/error-code");
 const fs = require("fs");
 const path = require("path");
 
+/**
+ * Generate `functionless.org/docs/error-codes file.
+ */
+
 const errorCodesAPIReference = path.join(
   __dirname,
   "..",
@@ -21,35 +25,31 @@ const errorCodeDocumentationPath = path.join(
   "error-codes.md"
 );
 
-(async function () {
-  let errorCodeMarkdown = (await fs.promises.readFile(errorCodesAPIReference))
-    .toString()
-    .replace("## Variables\n", "# Error Codes\n")
-    .replace(
-      `id: "ErrorCodes"
+let errorCodeMarkdown = fs
+  .readFileSync(errorCodesAPIReference)
+  .toString()
+  .replace("## Variables\n", "# Error Codes\n")
+  .replace(
+    `id: "ErrorCodes"
 title: "Namespace: ErrorCodes"
 sidebar_label: "ErrorCodes"
 sidebar_position: 0
 custom_edit_url: null`,
-      `title: "Error Codes"
+    `title: "Error Codes"
 sidebar_position: 3`
-    )
-    .replace(/• `Const`.*\n/g, "")
-    .replace(/\n\n\n/g, "\n\n")
-    .replace(/\n#### Defined in.*\n\n.*error.*\n/g, "");
+  )
+  .replace(/• `Const`.*\n/g, "")
+  .replace(/\n\n\n/g, "\n\n")
+  .replace(/\n#### Defined in.*\n\n.*error.*\n/g, "");
 
-  for (const [errorId, errorCode] of Object.entries(ErrorCodes)) {
-    //### Cannot\_perform\_arithmetic\_on\_variables\_in\_Step\_Function
-    errorCodeMarkdown = errorCodeMarkdown.replace(
-      `### ${errorId.replace(/_/g, "\\_")}`,
-      `### ${errorCode.messageText}
+for (const [errorId, errorCode] of Object.entries(ErrorCodes)) {
+  //### Cannot\_perform\_arithmetic\_on\_variables\_in\_Step\_Function
+  errorCodeMarkdown = errorCodeMarkdown.replace(
+    `### ${errorId.replace(/_/g, "\\_")}`,
+    `### ${errorCode.messageText}
 
 __Error Code__: Functionless(${errorCode.code})`
-    );
-  }
+  );
+}
 
-  await fs.promises.writeFile(errorCodeDocumentationPath, errorCodeMarkdown);
-})().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+fs.writeFileSync(errorCodeDocumentationPath, errorCodeMarkdown);
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -3,6 +3,7 @@
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const path = require("path");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -21,7 +22,6 @@ const config = {
   plugins: [
     [
       "docusaurus-plugin-typedoc",
-
       // Plugin / TypeDoc options
       {
         entryPoints: ["../src/index.ts"],
@@ -32,6 +32,19 @@ const config = {
         },
       },
     ],
+    function () {
+      return {
+        name: "functionless-error-code-docs",
+        loadContent: () =>
+          // run the compile-error-code-page CLI after typedoc is run by `docusaurus-plugin-typedoc`
+          require(path.join(
+            __dirname,
+            "..",
+            "scripts",
+            "compile-error-code-page"
+          )),
+      };
+    },
   ],
 
   presets: [
