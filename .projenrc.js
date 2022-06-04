3519fd75fb41d67d308237857a989184c5312bef
Sam Sussman <sussmansa@gmail.com>
2022-06-04 06:56:18 -0500

    fix: increase node memory

--- .projenrc.js
+++ .projenrc.js
@@ -103,7 +103,8 @@ const project = new CustomTypescriptProject({
   ],
   scripts: {
     localstack: "./scripts/localstack",
-    "build:website": "npx tsc && cd ./website && yarn && yarn build",
+    "build:website":
+      "NODE_OPTIONS=--max_old_space_size=4096 npx tsc && cd ./website && yarn && yarn build",
   },
   peerDeps: [
     `aws-cdk-lib@^${MIN_CDK_VERSION}`,
