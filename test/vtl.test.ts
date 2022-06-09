48e4074e6647e01a3c5b27adf4952436fd73d8ab
Sam Sussman <sussmansa@gmail.com>
2022-06-09 10:56:26 -0500

    chore: move app sync tests to snapshots

--- test/vtl.test.ts
+++ test/vtl.test.ts
@@ -1,20 +1,13 @@
 import "jest";
 import { $util, AppsyncContext } from "../src";
 import { reflect } from "../src/reflect";
-import { returnExpr, appsyncTestCase } from "./util";
-
-const payload = `{
-  "version": "2018-05-29",
-  "payload": null
-}`;
+import { appsyncTestCase } from "./util";
 
 test("empty function returning an argument", () => {
   appsyncTestCase(
     reflect((context: AppsyncContext<{ a: string }>) => {
       return context.arguments.a;
-    }),
-    payload,
-    "#return($context.arguments.a)"
+    })
   );
 });
 
@@ -37,22 +30,7 @@ test("return literal object with values", () => {
           ...obj,
         };
       }
-    ),
-    payload,
-    `#set($context.stash.arg = $context.arguments.arg)
-#set($context.stash.obj = $context.arguments.obj)
-#set($v1 = {})
-$util.qr($v1.put('null', $null))
-$util.qr($v1.put('undefined', $null))
-$util.qr($v1.put('string', 'hello'))
-$util.qr($v1.put('number', 1))
-$util.qr($v1.put('list', ['hello']))
-#set($v2 = {})
-$util.qr($v2.put('key', 'value'))
-$util.qr($v1.put('obj', $v2))
-$util.qr($v1.put('arg', $context.stash.arg))
-$util.qr($v1.putAll($context.stash.obj))
-#return($v1)`
+    )
   );
 });
 
@@ -67,15 +45,7 @@ test("computed property names", () => {
           [value]: context.arguments.arg,
         };
       }
-    ),
-    payload,
-    `#set($context.stash.name = $context.arguments.arg)
-#set($v1 = $context.stash.name + '_test')
-#set($context.stash.value = $v1)
-#set($v2 = {})
-$util.qr($v2.put($context.stash.name, $context.arguments.arg))
-$util.qr($v2.put($context.stash.value, $context.arguments.arg))
-#return($v2)`
+    )
   );
 });
 
@@ -88,12 +58,7 @@ test("null and undefined", () => {
           value: undefined,
         };
       }
-    ),
-    payload,
-    `#set($v1 = {})
-$util.qr($v1.put('name', $null))
-$util.qr($v1.put('value', $null))
-#return($v1)`
+    )
   );
 });
 
@@ -101,9 +66,7 @@ test("call function and return its value", () => {
   appsyncTestCase(
     reflect(() => {
       return $util.autoId();
-    }),
-    payload,
-    "#return($util.autoId())"
+    })
   );
 });
 
@@ -112,10 +75,7 @@ test("call function, assign to variable and return variable reference", () => {
     reflect(() => {
       const id = $util.autoId();
       return id;
-    }),
-    payload,
-    `#set($context.stash.id = $util.autoId())
-#return($context.stash.id)`
+    })
   );
 });
 
@@ -126,12 +86,7 @@ test("return in-line spread object", () => {
         id: $util.autoId(),
         ...context.arguments.obj,
       };
-    }),
-    payload,
-    `#set($v1 = {})
-$util.qr($v1.put('id', $util.autoId()))
-$util.qr($v1.putAll($context.arguments.obj))
-#return($v1)`
+    })
   );
 });
 
@@ -139,9 +94,7 @@ test("return in-line list literal", () => {
   appsyncTestCase(
     reflect((context: AppsyncContext<{ a: string; b: string }>) => {
       return [context.arguments.a, context.arguments.b];
-    }),
-    payload,
-    "#return([$context.arguments.a, $context.arguments.b])"
+    })
   );
 });
 
@@ -150,10 +103,7 @@ test("return list literal variable", () => {
     reflect((context: AppsyncContext<{ a: string; b: string }>) => {
       const list = [context.arguments.a, context.arguments.b];
       return list;
-    }),
-    payload,
-    `#set($context.stash.list = [$context.arguments.a, $context.arguments.b])
-#return($context.stash.list)`
+    })
   );
 });
 
@@ -162,10 +112,7 @@ test("return list element", () => {
     reflect((context: AppsyncContext<{ a: string; b: string }>) => {
       const list = [context.arguments.a, context.arguments.b];
       return list[0];
-    }),
-    payload,
-    `#set($context.stash.list = [$context.arguments.a, $context.arguments.b])
-#return($context.stash.list[0])`
+    })
   );
 });
 
@@ -174,10 +121,7 @@ test("push element to array is renamed to add", () => {
     reflect((context: AppsyncContext<{ list: string[] }>) => {
       context.arguments.list.push("hello");
       return context.arguments.list;
-    }),
-    payload,
-    `$util.qr($context.arguments.list.addAll(['hello']))
-#return($context.arguments.list)`
+    })
   );
 });
 
@@ -205,14 +149,7 @@ test("if statement", () => {
       } else {
         return false;
       }
-    }),
-    payload,
-    `#set($v1 = $context.arguments.list.length > 0)
-#if($v1)
-${returnExpr("true")}
-#else
-${returnExpr("false")}
-#end`
+    })
   );
 });
 
@@ -220,15 +157,7 @@ test("return conditional expression", () => {
   appsyncTestCase(
     reflect((context: AppsyncContext<{ list: string[] }>) => {
       return context.arguments.list.length > 0 ? true : false;
-    }),
-    payload,
-    `#set($v2 = $context.arguments.list.length > 0)
-#if($v2)
-#set($v1 = true)
-#else
-#set($v1 = false)
-#end
-#return($v1)`
+    })
   );
 });
 
@@ -238,17 +167,7 @@ test("property assignment of conditional expression", () => {
       return {
         prop: context.arguments.list.length > 0 ? true : false,
       };
-    }),
-    payload,
-    `#set($v1 = {})
-#set($v3 = $context.arguments.list.length > 0)
-#if($v3)
-#set($v2 = true)
-#else
-#set($v2 = false)
-#end
-$util.qr($v1.put('prop', $v2))
-#return($v1)`
+    })
   );
 });
 
@@ -260,13 +179,7 @@ test("for-of loop", () => {
         newList.push(item);
       }
       return newList;
-    }),
-    payload,
-    `#set($context.stash.newList = [])
-#foreach($item in $context.arguments.list)
-$util.qr($context.stash.newList.addAll([$item]))
-#end
-#return($context.stash.newList)`
+    })
   );
 });
 
@@ -281,17 +194,7 @@ test("break from for-loop", () => {
         newList.push(item);
       }
       return newList;
-    }),
-    payload,
-    `#set($context.stash.newList = [])
-#foreach($item in $context.arguments.list)
-#set($v1 = $item == 'hello')
-#if($v1)
-#break
-#end
-$util.qr($context.stash.newList.addAll([$item]))
-#end
-#return($context.stash.newList)`
+    })
   );
 });
 
@@ -304,14 +207,7 @@ test("local variable inside for-of loop is declared as a local variable", () => 
         newList.push(i);
       }
       return newList;
-    }),
-    payload,
-    `#set($context.stash.newList = [])
-#foreach($item in $context.arguments.list)
-#set($i = $item)
-$util.qr($context.stash.newList.addAll([$i]))
-#end
-#return($context.stash.newList)`
+    })
   );
 });
 
@@ -323,13 +219,7 @@ test("for-in loop and element access", () => {
         newList.push(context.arguments.record[key]);
       }
       return newList;
-    }),
-    payload,
-    `#set($context.stash.newList = [])
-#foreach($key in $context.arguments.record.keySet())
-$util.qr($context.stash.newList.addAll([$context.arguments.record[$key]]))
-#end
-#return($context.stash.newList)`
+    })
   );
 });
 
@@ -338,10 +228,7 @@ test("template expression", () => {
     reflect((context: AppsyncContext<{ a: string }>) => {
       const local = context.arguments.a;
       return `head ${context.arguments.a} ${local}${context.arguments.a}`;
-    }),
-    payload,
-    `#set($context.stash.local = $context.arguments.a)
-#return("head \${context.arguments.a} \${context.stash.local}\${context.arguments.a}")`
+    })
   );
 });
 
@@ -351,15 +238,7 @@ test("conditional expression in template expression", () => {
       return `head ${
         context.arguments.a === "hello" ? "world" : context.arguments.a
       }`;
-    }),
-    payload,
-    `#set($v2 = $context.arguments.a == 'hello')
-#if($v2)
-#set($v1 = 'world')
-#else
-#set($v1 = $context.arguments.a)
-#end
-#return("head \${v1}")`
+    })
   );
 });
 
@@ -369,28 +248,14 @@ test("map over list", () =>
       return context.arguments.list.map((item) => {
         return `hello ${item}`;
       });
-    }),
-    payload,
-    `#set($v1 = [])
-#foreach($item in $context.arguments.list)
-#set($v2 = \"hello \${item}\")
-$util.qr($v1.add($v2))
-#end
-#return($v1)`
+    })
   ));
 
 test("map over list with in-line return", () =>
   appsyncTestCase(
     reflect((context: AppsyncContext<{ list: string[] }>) => {
       return context.arguments.list.map((item) => `hello ${item}`);
-    }),
-    payload,
-    `#set($v1 = [])
-#foreach($item in $context.arguments.list)
-#set($v2 = \"hello \${item}\")
-$util.qr($v1.add($v2))
-#end
-#return($v1)`
+    })
   ));
 
 test("chain map over list", () =>
@@ -399,15 +264,7 @@ test("chain map over list", () =>
       return context.arguments.list
         .map((item) => `hello ${item}`)
         .map((item) => `hello ${item}`);
-    }),
-    payload,
-    `#set($v1 = [])
-#foreach($item in $context.arguments.list)
-#set($item = "hello \${item}")
-#set($v2 = "hello \${item}")
-$util.qr($v1.add($v2))
-#end
-#return($v1)`
+    })
   ));
 
 test("chain map over list multiple array", () =>
@@ -416,23 +273,7 @@ test("chain map over list multiple array", () =>
       return context.arguments.list
         .map((item, _i, _arr) => `hello ${item}`)
         .map((item, _i, _arr) => `hello ${item}`);
-    }),
-    payload,
-    `#set($v1 = [])
-#set($v2 = [])
-#foreach($item in $context.arguments.list)
-#set($_i = $foreach.index)
-#set($_arr = $context.arguments.list)
-#set($v3 = "hello \${item}")
-$util.qr($v2.add($v3))
-#end
-#foreach($item in $v2)
-#set($_i = $foreach.index)
-#set($_arr = $v2)
-#set($v4 = "hello \${item}")
-$util.qr($v1.add($v4))
-#end
-#return($v1)`
+    })
   ));
 
 test("chain map over list complex", () =>
@@ -444,20 +285,7 @@ test("chain map over list complex", () =>
           return `hello ${item} ${x} ${arr.length}`;
         })
         .map((item2, ii) => `hello ${item2} ${ii}`);
-    }),
-    payload,
-    `#set($v1 = [])
-#foreach($item in $context.arguments.list)
-#set($i = $foreach.index)
-#set($arr = $context.arguments.list)
-#set($v2 = $i + 1)
-#set($x = $v2)
-#set($item2 = "hello \${item} \${x} \${arr.length}")
-#set($ii = $foreach.index)
-#set($v3 = "hello \${item2} \${ii}")
-$util.qr($v1.add($v3))
-#end
-#return($v1)`
+    })
   ));
 
 test("forEach over list", () =>
@@ -466,12 +294,7 @@ test("forEach over list", () =>
       return context.arguments.list.forEach((item) => {
         $util.error(item);
       });
-    }),
-    payload,
-    `#foreach($item in $context.arguments.list)
-$util.qr($util.error($item))
-#end
-#return($null)`
+    })
   ));
 
 test("reduce over list with initial value", () =>
@@ -480,18 +303,7 @@ test("reduce over list with initial value", () =>
       return context.arguments.list.reduce((newList: string[], item) => {
         return [...newList, item];
       }, []);
-    }),
-    payload,
-    `#set($v1 = [])
-#foreach($item in $context.arguments.list)
-#set($newList = $v1)
-#set($v3 = [])
-$util.qr($v3.addAll($newList))
-$util.qr($v3.add($item))
-#set($v2 = $v3)
-#set($v1 = $v2)
-#end
-#return($v1)`
+    })
   ));
 
 test("reduce over list without initial value", () =>
@@ -500,21 +312,7 @@ test("reduce over list without initial value", () =>
       return context.arguments.list.reduce((str: string, item) => {
         return `${str}${item}`;
       });
-    }),
-    payload,
-    `#if($context.arguments.list.isEmpty())
-$util.error('Reduce of empty array with no initial value')
-#end
-#foreach($item in $context.arguments.list)
-#if($foreach.index == 0)
-#set($v1 = $item)
-#else
-#set($str = $v1)
-#set($v2 = \"\${str}\${item}\")
-#set($v1 = $v2)
-#end
-#end
-#return($v1)`
+    })
   ));
 
 test("map and reduce over list with initial value", () =>
@@ -525,19 +323,7 @@ test("map and reduce over list with initial value", () =>
         .reduce((newList: string[], item) => {
           return [...newList, item];
         }, []);
-    }),
-    payload,
-    `#set($v1 = [])
-#foreach($item in $context.arguments.list)
-#set($item = "hello \${item}")
-#set($newList = $v1)
-#set($v3 = [])
-$util.qr($v3.addAll($newList))
-$util.qr($v3.add($item))
-#set($v2 = $v3)
-#set($v1 = $v2)
-#end
-#return($v1)`
+    })
   ));
 
 test("map and reduce with array over list with initial value", () =>
@@ -548,25 +334,7 @@ test("map and reduce with array over list with initial value", () =>
         .reduce((newList: string[], item, _i, _arr) => {
           return [...newList, item];
         }, []);
-    }),
-    payload,
-    `#set($v2 = [])
-#foreach($item in $context.arguments.list)
-#set($v3 = "hello \${item}")
-$util.qr($v2.add($v3))
-#end
-#set($v1 = [])
-#foreach($item in $v2)
-#set($_i = $foreach.index)
-#set($_arr = $v2)
-#set($newList = $v1)
-#set($v5 = [])
-$util.qr($v5.addAll($newList))
-$util.qr($v5.add($item))
-#set($v4 = $v5)
-#set($v1 = $v4)
-#end
-#return($v1)`
+    })
   ));
 
 test("map and reduce and map and reduce over list with initial value", () =>
@@ -581,38 +349,14 @@ test("map and reduce and map and reduce over list with initial value", () =>
         .reduce((newList: string[], item) => {
           return [...newList, item];
         }, []);
-    }),
-    payload,
-    `#set($v2 = [])
-#foreach($item in $context.arguments.list)
-#set($item = "hello \${item}")
-#set($newList = $v2)
-#set($v4 = [])
-$util.qr($v4.addAll($newList))
-$util.qr($v4.add($item))
-#set($v3 = $v4)
-#set($v2 = $v3)
-#end
-#set($v1 = [])
-#foreach($item in $v2)
-#set($item = "hello \${item}")
-#set($newList = $v1)
-#set($v6 = [])
-$util.qr($v6.addAll($newList))
-$util.qr($v6.add($item))
-#set($v5 = $v6)
-#set($v1 = $v5)
-#end
-#return($v1)`
+    })
   ));
 
 test("$util.time.nowISO8601", () =>
   appsyncTestCase(
     reflect(() => {
       return $util.time.nowISO8601();
-    }),
-    payload,
-    "#return($util.time.nowISO8601())"
+    })
   ));
 
 test("BinaryExpr and UnaryExpr are evaluated to temporary variables", () =>
@@ -623,16 +367,5 @@ test("BinaryExpr and UnaryExpr are evaluated to temporary variables", () =>
         y: -(1 + 1),
         z: !(true && false),
       };
-    }),
-    payload,
-    `#set($v1 = {})
-#set($v2 = 0 - 1)
-$util.qr($v1.put('x', $v2))
-#set($v3 = 1 + 1)
-#set($v4 = 0 - $v3)
-$util.qr($v1.put('y', $v4))
-#set($v5 = true && false)
-#set($v6 = !$v5)
-$util.qr($v1.put('z', $v6))
-#return($v1)`
+    })
   ));
