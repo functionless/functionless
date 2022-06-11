14ec5f1e607db807702469a023e9649c53398012
Sam Sussman <sussmansa@gmail.com>
2022-06-10 21:23:21 -0500

    feedback

--- src/error-code.ts
+++ src/error-code.ts
@@ -1,3 +1,7 @@
+const BASE_URL = process.env.FUNCTIONLESS_LOCAL
+  ? `http://localhost:3000`
+  : `https://functionless.org`;
+
 /**
  * Error to throw during synth failures
  */
@@ -6,21 +10,33 @@ export class SynthError extends Error {
     super(formatErrorMessage(code, message));
   }
 }
-const baseUrl = process.env.FUNCTIONLESS_LOCAL
-  ? `http://localhost:3000`
-  : `https://functionless.org`;
 
+/**
+ * Formats an error message consistently across Functionless.
+ *
+ * Includes a deep link url to functionless.org's error code page.
+ *
+ * ```
+ * [messageText | code.MessageText]
+ *
+ * http://functionless.org/docs/error-codes/#[Anchor from Message Text]
+ * ```
+ */
 export const formatErrorMessage = (code: ErrorCode, messageText?: string) => `${
   messageText ?? code.messageText
 }
 
 ${formatErrorUrl(code)}`;
 
-const formatErrorUrl = (code: ErrorCode) => {
-  return `${baseUrl}/docs/error-codes#${code.messageText
+/**
+ * Deep link to functionless.org's error code page.
+ *
+ * `http://functionless.org/docs/error-codes/#[Anchor from Message Text]`
+ */
+export const formatErrorUrl = (code: ErrorCode) =>
+  `${BASE_URL}/docs/error-codes#${code.messageText
     .toLowerCase()
     .replace(/\s/g, "-")}`;
-};
 
 export interface ErrorCode {
   code: number;
