9913903c11a61740e2a124a7338dd792215bc958
Sam Sussman <sussmansa@gmail.com>
2022-07-25 22:17:54 -0500

    feedback

--- src/step-function.ts
+++ src/step-function.ts
@@ -557,6 +557,60 @@ abstract class BaseStepFunction<
     });
   }
 
+  readonly apiGWVtl: ApiGatewayVtlIntegration = {
+    renderRequest: (call, context) => {
+      const args = retrieveMachineArgs(call);
+
+      return `{\n"stateMachineArn":"${
+        this.resource.stateMachineArn
+      }",\n${Object.entries(args)
+        .filter(
+          (arg): arg is [typeof arg[0], Exclude<typeof arg[1], undefined>] =>
+            arg[1] !== undefined
+        )
+        .map(([argName, argVal]) => {
+          if (argName === "input") {
+            // stringify the JSON input
+            return `"${argName}":"$util.escapeJavaScript(${context.exprToJson(
+              argVal
+            )})"`;
+          } else {
+            return `"${argName}":${context.exprToJson(argVal)}`;
+          }
+        })
+        .join(",")}\n}`;
+    },
+
+    createIntegration: (options) => {
+      const credentialsRole = options.credentialsRole;
+
+      this.resource.grantRead(credentialsRole);
+      if (
+        this.resource.stateMachineType ===
+        aws_stepfunctions.StateMachineType.EXPRESS
+      ) {
+        this.resource.grantStartSyncExecution(credentialsRole);
+      } else {
+        this.resource.grantStartExecution(credentialsRole);
+      }
+
+      return new aws_apigateway.AwsIntegration({
+        service: "states",
+        action:
+          this.resource.stateMachineType ===
+          aws_stepfunctions.StateMachineType.EXPRESS
+            ? "StartSyncExecution"
+            : "StartExecution",
+        integrationHttpMethod: "POST",
+        options: {
+          ...options,
+          credentialsRole,
+          passthroughBehavior: aws_apigateway.PassthroughBehavior.NEVER,
+        },
+      });
+    },
+  };
+
   public appSyncIntegration(
     integration: Pick<AppSyncVtlIntegration, "request">
   ): AppSyncVtlIntegration {
@@ -998,50 +1052,6 @@ export class ExpressStepFunction<
 > extends BaseExpressStepFunction<Payload, Out> {
   readonly definition: StateMachine<States>;
 
-  // Integration object for api gateway vtl
-  readonly apiGWVtl: ApiGatewayVtlIntegration = {
-    renderRequest: (call, context) => {
-      const args = retrieveMachineArgs(call);
-
-      return `{\n"stateMachineArn":"${
-        this.resource.stateMachineArn
-      }",\n${Object.entries(args)
-        .filter(
-          (arg): arg is [typeof arg[0], Exclude<typeof arg[1], undefined>] =>
-            arg[1] !== undefined
-        )
-        .map(([argName, argVal]) => {
-          if (argName === "input") {
-            // stringify the JSON input
-            return `"${argName}":"$util.escapeJavaScript(${context.exprToJson(
-              argVal
-            )})"`;
-          } else {
-            return `"${argName}":${context.exprToJson(argVal)}`;
-          }
-        })
-        .join(",")}\n}`;
-    },
-
-    createIntegration: (options) => {
-      const credentialsRole = options.credentialsRole;
-
-      this.resource.grantRead(credentialsRole);
-      this.resource.grantStartSyncExecution(credentialsRole);
-
-      return new aws_apigateway.AwsIntegration({
-        service: "states",
-        action: "StartSyncExecution",
-        integrationHttpMethod: "POST",
-        options: {
-          ...options,
-          credentialsRole,
-          passthroughBehavior: aws_apigateway.PassthroughBehavior.NEVER,
-        },
-      });
-    },
-  };
-
   /**
    * Wrap a {@link aws_stepfunctions.StateMachine} with Functionless.
    *
@@ -1272,49 +1282,6 @@ class BaseStandardStepFunction<
       },
     };
   }
-
-  readonly apiGWVtl: ApiGatewayVtlIntegration = {
-    renderRequest: (call, context) => {
-      const args = retrieveMachineArgs(call);
-
-      return `{\n"stateMachineArn":"${
-        this.resource.stateMachineArn
-      }",\n${Object.entries(args)
-        .filter(
-          (arg): arg is [typeof arg[0], Exclude<typeof arg[1], undefined>] =>
-            arg[1] !== undefined
-        )
-        .map(([argName, argVal]) => {
-          if (argName === "input") {
-            // stringify the JSON input
-            return `"${argName}":"$util.escapeJavaScript(${context.exprToJson(
-              argVal
-            )})"`;
-          } else {
-            return `"${argName}":${context.exprToJson(argVal)}`;
-          }
-        })
-        .join(",")}\n}`;
-    },
-
-    createIntegration: (options) => {
-      const credentialsRole = options.credentialsRole;
-
-      this.resource.grantRead(credentialsRole);
-      this.resource.grantStartExecution(credentialsRole);
-
-      return new aws_apigateway.AwsIntegration({
-        service: "states",
-        action: "StartExecution",
-        integrationHttpMethod: "POST",
-        options: {
-          ...options,
-          credentialsRole,
-          passthroughBehavior: aws_apigateway.PassthroughBehavior.NEVER,
-        },
-      });
-    },
-  };
 
   public describeExecution = makeIntegration<
     "StepFunction.describeExecution",
