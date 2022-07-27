360586f2c39122fe4b2e601e5c7c6d5a2e2de884
sam <sam@functionless.org>
2022-07-27 10:50:47 -0700

    chore: feedback

--- src/declaration.ts
+++ src/declaration.ts
@@ -267,10 +267,6 @@ export class ObjectBinding extends BaseNode<"ObjectBinding", VariableDecl> {
   public clone(): this {
     return new ObjectBinding(this.bindings.map((b) => b.clone())) as this;
   }
-
-  public getName(): never {
-    throw new Error(`an ObjectBinding does not have a name`);
-  }
 }
 
 /**
@@ -302,10 +298,6 @@ export class ArrayBinding extends BaseNode<"ArrayBinding", VariableDecl> {
 
   public clone(): this {
     return new ArrayBinding(this.bindings.map((b) => b?.clone())) as this;
-  }
-
-  public getName(): never {
-    throw new Error(`an ArrayBinding does not have a name`);
   }
 }
 
