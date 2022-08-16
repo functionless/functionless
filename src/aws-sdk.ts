9d43d371dc1ad307a5a574e88868eeccc1a71726
Matthias Pichler <m.pichler@warrify.com>
2022-08-16 09:44:36 +0000

    refactor: split sdk types into new file to reuse them

---
+++ src/aws-sdk.ts
@@ -0,0 +1,98 @@
+import type { Service as AWSService } from "aws-sdk";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import type * as AWS from "aws-sdk";
+import { AnyFunction, OverloadUnion } from "./util";
+
+type AWSServiceClass = { new (): AWSService };
+
+/**
+ * First we have to extract the names of all Services in the v2 AWS namespace
+ *
+ * @returns "AccessAnalyzer" | "Account" | ... | "XRay"
+ */
+export type ServiceKeys = Exclude<
+  {
+    [K in keyof typeof AWS]: typeof AWS[K] extends AWSServiceClass ? K : never;
+  }[keyof typeof AWS],
+  "Service"
+>;
+
+export type SDK = {
+  [serviceName in ServiceKeys]: typeof AWS[serviceName] extends new (
+    ...args: any[]
+  ) => infer Client
+    ? {
+        [methodName in keyof Client]: SdkMethod<Client[methodName]>;
+      }
+    : never;
+};
+
+export interface SdkCallOptions {
+  /**
+   * The resources for the IAM statement that will be added to the state machine
+   * role's policy to allow the state machine to make the API call.
+   *
+   * Use `["*"]` to grant access to all resources (discouraged).
+   *
+   * @example ["arn:aws:s3:::my_bucket"]
+   * @see https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_resource.html
+   */
+  iamResources: string[];
+
+  /**
+   * The action for the IAM statement that will be added to the state machine role's
+   * policy to allow the state machine to make the API call.
+   *
+   * By default the action is inferred from the API call (e.g. `$AWS.SDK.CloudWatch.describeAlarms({})` results in `cloudwatch:DescribeAlarms`)
+   *
+   * Use in the case where the IAM action name does not match with the API service/action name
+   * e.g. `s3:ListBuckets` requires `s3:ListAllMyBuckets`.
+   *
+   * To see which action to use with which service/method see the {@link https://docs.aws.amazon.com/service-authorization/latest/reference/reference_policies_actions-resources-contextkeys.html | IAM Service Authorization Reference}
+   *
+   * @default service:method
+   * @example s3:ListAllMyBuckets
+   */
+  iamActions?: string[];
+
+  /**
+   * The iam conditions to apply to the IAM Statement that will be added to the state machine role's
+   * policy to allow the state machine to make the API call.
+   *
+   * By default no conditions are applied.
+   *
+   * For more information check out the {@link https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_condition.html | IAM User Guide}
+   *
+   * @example
+   * ```
+   * {
+   *   "StringEquals" : { "aws:username" : "johndoe" }
+   * }
+   * ```
+   */
+  iamConditions?: Record<string, any>;
+
+  /**
+   * The service name to use in the ASL (Amazon States Language) definition of the SDK integration.
+   * ```json
+   * {
+   *   "Type": "Task",
+   *   "Resource": "arn:aws:states:::aws-sdk:${sdkIntegrationServiceName}:${methodName}"
+   * }
+   * ```
+   *
+   * By default the service name is lowercased (e.g. `$AWS.SDK.CloudWatch.describeAlarms` => `"arn:aws:states:::aws-sdk:cloudwatch:describeAlarms"`).
+   * Some known special cases are also handled in {@link SDK_INTEGRATION_SERVICE_NAME}.
+   *
+   * To see which service name to use see the {@link https://docs.aws.amazon.com/step-functions/latest/dg/supported-services-awssdk.html#supported-services-awssdk-list | StepFunctions Developer Guide}
+   */
+  aslServiceName?: string;
+}
+
+export type SdkMethod<API> = API extends AnyFunction
+  ? Exclude<OverloadUnion<API>, (cb: AnyFunction) => any> extends (
+      input: infer Input extends {}
+    ) => AWS.Request<infer Output, any>
+    ? (input: Input, options: SdkCallOptions) => Promise<Output>
+    : never
+  : never;
--- src/aws.ts
+++ src/aws.ts
@@ -3,7 +3,6 @@ import type {
   DynamoDB as AWSDynamoDB,
   EventBridge as AWSEventBridge,
   Lambda as AWSLambda,
-  Service as AWSService,
 } from "aws-sdk";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as AWS from "aws-sdk";
@@ -22,7 +21,8 @@ import {
   UpdateItemInput,
   UpdateItemOutput,
 } from "typesafe-dynamodb/lib/update-item";
-import { ASLGraph } from "./asl";
+import { ASLGraph, SDK_INTEGRATION_SERVICE_NAME } from "./asl";
+import { ServiceKeys, SDK, SdkCallOptions } from "./aws-sdk";
 import { ErrorCodes, SynthError } from "./error-code";
 import { Argument, Expr } from "./expression";
 import { Function, isFunction, NativeIntegration } from "./function";
@@ -42,12 +42,7 @@ import {
   makeIntegration,
 } from "./integration";
 import { AnyTable, isTable, ITable } from "./table";
-import {
-  AnyFunction,
-  AnyAsyncFunction,
-  OverloadUnion,
-  evalToConstant,
-} from "./util";
+import { AnyAsyncFunction, evalToConstant } from "./util";
 
 /**
  * The `AWS` namespace exports functions that map to AWS Step Functions AWS-SDK Integrations.
@@ -650,118 +645,6 @@ export namespace $AWS {
     }
   );
 }
-
-type AWSServiceClass = { new (): AWSService };
-
-/**
- * First we have to extract the names of all Services in the v2 AWS namespace
- *
- * @returns "AccessAnalyzer" | "Account" | ... | "XRay"
- */
-type ServiceKeys = Exclude<
-  {
-    [K in keyof typeof AWS]: typeof AWS[K] extends AWSServiceClass ? K : never;
-  }[keyof typeof AWS],
-  "Service"
->;
-
-type SDK = {
-  [serviceName in ServiceKeys]: typeof AWS[serviceName] extends new (
-    ...args: any[]
-  ) => infer Client
-    ? {
-        [methodName in keyof Client]: SdkMethod<Client[methodName]>;
-      }
-    : never;
-};
-
-interface SdkCallOptions {
-  /**
-   * The resources for the IAM statement that will be added to the state machine
-   * role's policy to allow the state machine to make the API call.
-   *
-   * Use `["*"]` to grant access to all resources (discouraged).
-   *
-   * @example ["arn:aws:s3:::my_bucket"]
-   * @see https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_resource.html
-   */
-  iamResources: string[];
-
-  /**
-   * The action for the IAM statement that will be added to the state machine role's
-   * policy to allow the state machine to make the API call.
-   *
-   * By default the action is inferred from the API call (e.g. `$AWS.SDK.CloudWatch.describeAlarms({})` results in `cloudwatch:DescribeAlarms`)
-   *
-   * Use in the case where the IAM action name does not match with the API service/action name
-   * e.g. `s3:ListBuckets` requires `s3:ListAllMyBuckets`.
-   *
-   * To see which action to use with which service/method see the {@link https://docs.aws.amazon.com/service-authorization/latest/reference/reference_policies_actions-resources-contextkeys.html | IAM Service Authorization Reference}
-   *
-   * @default service:method
-   * @example s3:ListAllMyBuckets
-   */
-  iamActions?: string[];
-
-  /**
-   * The iam conditions to apply to the IAM Statement that will be added to the state machine role's
-   * policy to allow the state machine to make the API call.
-   *
-   * By default no conditions are applied.
-   *
-   * For more information check out the {@link https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_condition.html | IAM User Guide}
-   *
-   * @example
-   * ```
-   * {
-   *   "StringEquals" : { "aws:username" : "johndoe" }
-   * }
-   * ```
-   */
-  iamConditions?: Record<string, any>;
-
-  /**
-   * The service name to use in the ASL (Amazon States Language) definition of the SDK integration.
-   * ```json
-   * {
-   *   "Type": "Task",
-   *   "Resource": "arn:aws:states:::aws-sdk:${sdkIntegrationServiceName}:${methodName}"
-   * }
-   * ```
-   *
-   * By default the service name is lowercased (e.g. `$AWS.SDK.CloudWatch.describeAlarms` => `"arn:aws:states:::aws-sdk:cloudwatch:describeAlarms"`).
-   * Some known special cases are also handled in {@link SDK_INTEGRATION_SERVICE_NAME}.
-   *
-   * To see which service name to use see the {@link https://docs.aws.amazon.com/step-functions/latest/dg/supported-services-awssdk.html#supported-services-awssdk-list | StepFunctions Developer Guide}
-   */
-  aslServiceName?: string;
-}
-
-type SdkMethod<API> = API extends AnyFunction
-  ? Exclude<OverloadUnion<API>, (cb: AnyFunction) => any> extends (
-      input: infer Input extends {}
-    ) => AWS.Request<infer Output, any>
-    ? (input: Input, options: SdkCallOptions) => Promise<Output>
-    : never
-  : never;
-
-const SDK_INTEGRATION_SERVICE_NAME: Partial<Record<ServiceKeys, string>> = {
-  Discovery: "applicationdiscovery",
-  ConfigService: "config",
-  CUR: "costandusagereport",
-  DMS: "databasemigration",
-  DirectoryService: "directory",
-  MarketplaceEntitlementService: "marketplaceentitlement",
-  RDSDataService: "rdsdata",
-  StepFunctions: "sfn",
-  AugmentedAIRuntime: "sagemakera2iruntime",
-  ForecastQueryService: "forecastquery",
-  KinesisVideoSignalingChannels: "kinesisvideosignaling",
-  LexModelBuildingService: "lexmodelbuilding",
-  TranscribeService: "transcribe",
-  ELB: "elasticloadbalancing",
-  ELBv2: "elasticloadbalancingv2",
-};
 
 export type OperationName =
   | "deleteItem"
