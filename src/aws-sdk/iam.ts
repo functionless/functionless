1b48c92567831d4900c35093ceaeffc6c3380edc
sam <sam@functionless.org>
2022-09-01 19:27:25 -0700

    feat: add ControlTower to IAM_SERVICE_PREFIX

--- src/aws-sdk/iam.ts
+++ src/aws-sdk/iam.ts
@@ -75,6 +75,7 @@ export const IAM_SERVICE_PREFIX: Record<keyof SDK, string> = {
   ConnectCampaigns: "connect-campaigns",
   ConnectContactLens: "connect",
   ConnectParticipant: "connect",
+  ControlTower: "controltower",
   CostExplorer: "ce",
   CUR: "cur",
   CustomerProfiles: "profile",
