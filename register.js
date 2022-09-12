25226359a0637df65b957207478687baffb8f7a7
Tyler van Hensbergen <tyler@functionless.org>
2022-09-12 11:39:14 -0600

    incorporate hook.js changes from sqs PR

--- register.js
+++ register.js
@@ -1,4 +1,18 @@
 const register = require("@swc/register/lib/node").default;
 const { config } = require("./lib/swc");
+const path = require("path");
 
-register(config);
+const src = path.resolve("src");
+const functionlessLib = path.resolve(__dirname, "lib");
+register({
+  ...config,
+  ignore: [
+    (file) => {
+      if (file.startsWith(src) || file.startsWith(functionlessLib)) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  ],
+});
