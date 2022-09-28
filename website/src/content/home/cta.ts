ed5f6e169a5a12081e914a2caa786d478c13c51c
Chris Fraser <chris@functionless.org>
2022-09-28 05:31:45 -0600

    change bottom docs link to getting started

--- website/src/content/home/cta.ts
+++ website/src/content/home/cta.ts
@@ -4,6 +4,6 @@ export const body =
   "Functionless is an open source framework. Build your first cloud application today - all you need is an AWS account.";
 
 export const docsButton = {
-  to: "/docs/what-is-functionless",
+  to: "/docs/getting-started/setup",
   title: "Read Docs",
 };
