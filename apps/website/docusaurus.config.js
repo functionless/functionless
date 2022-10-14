9735564c8d37adf27df4234249778313a26154b8
sam <sam@functionless.org>
2022-10-13 19:53:52 -0700

    chore: set up googlge analytics

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -73,6 +73,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        googleAnalytics: {
+          anonymizeIP: true,
+          trackingID: "G-PYETWN9YYZ",
+        },
       }),
     ],
   ],
